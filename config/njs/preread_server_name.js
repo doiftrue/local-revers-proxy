288e76cd14f7464608540f6140aced9234c06e45
Kama <tkama@bk.ru>
2024-08-11 19:43:58 +0500

    IMP: simplification for sites.conf (upstreams removed)

--- config/njs/preread_server_name.js
+++ config/njs/preread_server_name.js
@@ -4,21 +4,21 @@ let server_name = '-';
  * Read the server name from the HTTP stream.
  */
 function read_server_name( stream ){
-	stream.on( 'upload', function( data, flags ){
-		if( data.length || flags.last ){
-			stream.done();
-		}
+  stream.on( 'upload', function( data, flags ){
+    if( data.length || flags.last ){
+      stream.done();
+    }
 
-		// Get "Host". Without port (:)
-		let match = data.match( /\r?\nHost: ([^\r\n:]+)/ );
-		if( match ){
-			server_name = match[1];
-		}
-	} );
+    // Get "Host". Without port (:)
+    let match = data.match( /\r?\nHost: ([^\r\n:]+)/ );
+    if( match ){
+      server_name = match[1];
+    }
+  } );
 }
 
 function get_server_name( stream ){
-	return server_name
+  return server_name
 }
 
 export default { read_server_name, get_server_name }
